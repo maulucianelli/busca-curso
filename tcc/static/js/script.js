7bd4bc67387c7fc78bc5e5bbfd375a1469af5cef
Mauricio Lucianelli Junior <36964424+maulucianelli@users.noreply.github.com>
2021-10-27 22:50:28 -0300

    UPDATE BUSCA LISTA DE CURSOS

--- tcc/static/js/script.js
+++ tcc/static/js/script.js
@@ -23,23 +23,38 @@ function showSalary(response){
   document.getElementById("salario_senior").innerText += response[1].SALARIO_SENIOR
   console.log("heloo")
 }
+var $rows = $('#table tr');
+$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+    
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
 
-function myFunction(){
+function searchArticles() {
+  search('searchArticles', 'articles');
+}
+  
+function search(searchInput, searchTable) {
+  // Declare variables
   var input, filter, table, tr, td, i, txtValue;
-  input =  document.getElementById("myInput");
+  input = document.getElementById(searchInput);
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById(searchTable);
   tr = table.getElementsByTagName("tr");
-
-  for (i = 0; i < tr.length; i++){
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if(txtValue.toUpperCase().indexOf(filter) > -1){
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+  
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          } else {
+              tr[i].style.display = "none";
+          }
       }
-    }
   }
 }
