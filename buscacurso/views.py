0960e5af93e9c91feb5d16667634f75d2b45b98a
Mauricio Lucianelli Junior <36964424+maulucianelli@users.noreply.github.com>
2021-11-08 21:15:28 -0300

    atualização views

--- buscacurso/views.py
+++ buscacurso/views.py
@@ -20,6 +20,7 @@ def index (request):
 
     return render(request, 'course_list.html',dados)
 def course_list(request):
+    #MyModel.objects.filter(blah=blah).first()
     courses_inst= CoursesInstitution.objects.all
     courses=Courses.objects.all
     institution=Institution.objects.all
